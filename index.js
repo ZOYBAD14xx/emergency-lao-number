05d9132f5ea6c8bb41dc23784e69c801b8b9b857
ZOYBAD14xx <60214442+ZOYBAD14xx@users.noreply.github.com>
2021-08-30 23:14:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const number = require("emergencylaonumber"); // import the installed package
+module.exports = async function getContests() {
+    const axios = require("axios"); // Importing the Axios module to make API requests
+    var result;
 
-number()
-    .then((res) => {
-        // If the function successfully retrieves the data, it enters this block
-        console.log(res); // Print the contest data on the console
-    })
-    .catch((err) => {
-        console.log(err); // Error handler
-    });
+    await axios // Making a GET request using axios and requesting information from the API
+        .get(
+            "https://raw.githubusercontent.com/ZOYBAD14xx/FreeAPI/main/EmergencyNumberLaos.json"
+        )
+        .then((response) => { // If the GET request is successful, this block is executed
+            return response; // The response of the API call is passed on to the next block
+        })
+        .then((contests) => { // In this block, we store the response data into a variable 'result'
+            result = contests.data;
+        })
+        .catch((err) => {
+            console.log(err); // Error handler
+        });
+    return result; // The contest data is returned
+};
